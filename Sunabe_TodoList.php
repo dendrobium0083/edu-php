f62acb1bc2e0748711ec23c192e722dae5b47e02
MakibaNoMochanMilk <rsunabe@abridge-co.jp>
2020-06-19 15:53:21 +0900

    Sunabe:TodoListSource

---
+++ Sunabe_TodoList.php
@@ -0,0 +1,39 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Todoリスト</title>
+</head>
+<body>
+    <h1>TODOリスト</h1>
+    <form action="TodoList.php" method="post">
+    <input type="text" name="name">
+    <input type="submit" value="追加">
+    </form>
+
+    <?php
+
+    session_start();
+    $todoList = array();
+
+    if(isset($_SESSION['data'])){
+        $todoList = $_SESSION['data'];
+    }
+        if(isset($_POST['name'])){
+            $todoList[] = $_POST['name'];
+        }
+        $_SESSION['data'] = $todoList;
+        
+        foreach($todoList as $list) {
+            // print_r($todoList);
+
+            // var_dump(implode($todoList));
+
+            // echo implode('<br>', $list);
+
+            echo "<p>$list</p>";
+        }
+    
+?>
+
+</body>
+</html>
