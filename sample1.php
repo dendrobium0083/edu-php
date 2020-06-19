918d56826f6e6c2947329ccb29b197147302cebd
onitsuka <dendrobium.onioni@gmail.com>
2020-06-19 09:54:25 +0900

    sanpur

---
+++ sample1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $a = 5 + 5;
+    $b = 5 - 5;
+    $c = 5 * 5;
+    $d = 5 / 5;
+    $e = 5 % 3;
+    $f = 5 ** 3;
+    
+    print "5+5 は$a<br>";
+    print "5-5 は$b<br>";
+    print "5*5 は$c<br>";
+    print "5/5 は$d<br>";
+    print "5%5 は$e<br>";
+    print "5**5 は$f<br>";
+    ?>
+</body>
+</html>
